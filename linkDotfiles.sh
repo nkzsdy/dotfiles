6ae0def25299979ead525ff135db5ed1de128a2c
Ryota Sakurai <nkzsdy@gmail.com>
2023-03-01 02:34:35 +0900

    replace setup script

--- linkDotfiles.sh
+++
@@ -1,4 +0,0 @@
-ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+# modify key repeat settings
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
+
+# setup prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# setup homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/nkzsdy/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew upgrade
+brew update
+
+# install command line tools & applications
+brew bundle
+
+# link dotfiles
+ln -sf ~/dotfiles/zsh/.zshrc     ~/.zshrc
+ln -sf ~/dotfiles/zsh/.zlogin    ~/.zlogin
+ln -sf ~/dotfiles/zsh/.zlogout   ~/.zlogout
+ln -sf ~/dotfiles/zsh/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/zsh/.zprofile  ~/.zprofile
+ln -sf ~/dotfiles/zsh/.zshenv    ~/.zshenv
+ln -sf ~/dotfiles/.gitconfig     ~/.gitconfig
+ln -sf ~/dotfiles/.tigrc         ~/.tigrc
+ln -sf ~/dotfiles/vim/.vimrc     ~/.vimrc
